b45c5bae69ad9a3dfb0aedf27aebddb54a9be609
Clayton Leach <zeerus@gmail.com>
2017-09-28 21:20:16 -0600

    Removed dialog from App.js, this will handle state of the backend.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React, { Component } from 'react';
 import TODOModule from './TODOModule.js'
 import './App.css'
 
-class Dialog extends Component {
-    render(){
-        return (
-            <div>
-            </div>
-        );
-    }
-}
-
 class App extends Component{
     render(){
         return(
