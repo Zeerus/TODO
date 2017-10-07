22e77b29b47eca563cbd7cee85fcb4cb69617b5c
Clayton Leach <zeerus@gmail.com>
2017-10-06 21:18:03 -0600

    Removed unecessary code causing a warning on compile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import TODOModule from './TODOModule.js';
-import FullScreenDialog from './FullScreenDialog.js'
 import './App.css';
 
 class App extends Component{
